4eda20d62c6bbfa2da1b2486caf01c4e203a51d5
Wil Boayue <wil.boayue@gmail.com>
2016-06-18 15:24:26 -0400

    iteration 1

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,16 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
+    base_bag = base |> word_bag
 
+    candidates
+    |> Enum.filter(&(String.downcase(base) != String.downcase(&1) && word_bag(&1) == base_bag))
+  end
+
+  defp word_bag(word) do
+    word
+    |> String.downcase
+    |> String.codepoints
+    |> Enum.sort
   end
 end
