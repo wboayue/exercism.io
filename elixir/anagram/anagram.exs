082579f03f5f272353f1af182a71b0954c9b5245
Wil Boayue <wil.boayue@gmail.com>
2016-06-18 23:24:54 -0400

    refactor anagram?

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,26 +4,24 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-    match(base, letter_list_sorted(base), candidates)
+    base = String.downcase(base)
+    match(base, sorted(base), candidates)
   end
 
-  defp match(base, base_list_sorted, candidates) do
+  defp match(base, base_sorted, candidates) do
     candidates
-    |> Enum.filter(&(!equal_ignore_case?(base, &1) && anagram?(base_list_sorted, &1)))
+    |> Enum.filter(&(anagram?(base, String.downcase(&1), base_sorted)))
   end
 
-  defp equal_ignore_case?(a, b) do
-    String.downcase(a) == String.downcase(b)
-  end
-
-  defp anagram?(base_list_sorted, word) do
-    base_list_sorted == letter_list_sorted(word) 
-  end
-
-  defp letter_list_sorted(word) do
+  defp sorted(word) do
     word
-    |> String.downcase
     |> String.to_char_list
     |> Enum.sort
   end
+
+  defp anagram?(a, a, _), do: false
+  defp anagram?(base, word, base_sorted) do
+    base_sorted == sorted(word) 
+  end
+
 end
