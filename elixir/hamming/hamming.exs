da51449ae0d407e48ecfba5cd49ad3089f8504ec
Wil Boayue <wboayue@targetedvictory.com>
2016-06-20 17:01:29 -0400

    cleanup typo

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -12,10 +12,10 @@ defmodule DNA do
   def hamming_distance(strand1, strand2) when length(strand1) != length(strand2), do: nil
   def hamming_distance(strand1, strand2) do
     Enum.zip(strand1, strand2)
-    |> Enum.map(&compare_nucleotide/1)
+    |> Enum.map(&compare_nucleotides/1)
     |> Enum.sum
   end
 
-  defp compare_nucleotide({x, x}), do: 0
-  defp compare_nucleotide({x, y}), do: 1
+  defp compare_nucleotides({x, x}), do: 0
+  defp compare_nucleotides({x, y}), do: 1
 end
