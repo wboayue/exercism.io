6fd8f0edbe2b698ad284db4dc31b8f80f12370c0
Wil Boayue <wil.boayue@gmail.com>
2016-06-21 14:16:22 -0400

    refactor

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -2,15 +2,22 @@ var Year = function(year) {
   this.year = year;
 };
 
+var LEAP_YEAR = true,
+    COMMON_YEAR = false;
+
+var isDivisibleBy = function isDivisibleBy(year, denominator) {
+  return year % denominator == 0
+}
+
 Year.prototype.isLeap = function() {
-  if (this.year % 4 !== 0) {
-    return false;
-  } else if (this.year % 100 !== 0) {
-    return true;
-  } else if (this.year % 400 !== 0) {
-    return false;
+  if (!isDivisibleBy(this.year, 4)) {
+    return COMMON_YEAR;
+  } else if (!isDivisibleBy(this.year, 100)) {
+    return LEAP_YEAR;
+  } else if (!isDivisibleBy(this.year, 400)) {
+    return COMMON_YEAR;
   } else {
-    return true;
+    return LEAP_YEAR;
   }
 };
 
