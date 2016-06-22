40a21f40bb164c15a294aa1c6688864afa5472f1
Wil Boayue <wboayue@targetedvictory.com>
2016-06-21 23:33:24 -0400

    checkpoint

---
+++ scala/bob/src/main/scala/bob.scala
@@ -0,0 +1,25 @@
+class Bob {
+
+  def hey(phrase: String): String = phrase.trim() match {
+    case phrase if silence(phrase) => "Fine. Be that way!"
+    case phrase if shouting(phrase) => "Whoa, chill out!"
+    case phrase if questioning(phrase) => "Sure."
+    case _ => "Whatever."
+  }
+
+  def silence(phrase: String): Boolean = {
+    phrase == ""
+  }
+
+  def has_letter(phrase: String): Boolean = {
+    "\\p{L}+".r == phrase
+  }
+
+  def shouting(phrase: String): Boolean = {
+    has_letter(phrase) && phrase.toUpperCase == phrase
+  }
+
+  def questioning(phrase: String): Boolean = {
+    phrase.endsWith("?")
+  }
+}
