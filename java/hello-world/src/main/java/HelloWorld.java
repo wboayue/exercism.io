eb5b22a5a9b4945d9bc4a72786dd3993fa42057a
Wil Boayue <wboayue@targetedvictory.com>
2016-06-23 12:37:07 -0400

    java:hello-world iteration 1

---
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+  public static String hello(String name) {
+    if (name == null || name.isEmpty()) {
+      name = "World";
+    }
+
+    return String.format("Hello, %s!", name);
+  }
+
+}
