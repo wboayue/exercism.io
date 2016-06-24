2d4936b24022a330c744e2dc57a8561add052b69
Wil Boayue <wboayue@targetedvictory.com>
2016-06-24 12:57:40 -0400

    scala:word-count iteration 1

---
+++ scala/word-count/src/main/scala/phrase.scala
@@ -0,0 +1,23 @@
+class Phrase(text: String) {
+
+  def wordCount(): Map[String, Int] = {
+    countWords(splitIntoWords(text))
+  }
+
+  private def countWords(words: List[String]): Map[String, Int] = {
+    words.groupBy(w => w).mapValues(_.size)
+  }
+
+  def splitIntoWords(phrase: String): List[String] = {
+    stripPunctuation(phrase).
+      toLowerCase().
+      split("[ _,]").
+      filter(w => w.size > 0).
+      toList
+  }
+
+  def stripPunctuation(phrase: String): String = {
+    phrase.trim.replaceAll("[:!&@$%^.]", "")
+  }
+
+}
