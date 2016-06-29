be78f160b023a7b9a67dfd6497db173ab500b400
Wil Boayue <wboayue@targetedvictory.com>
2016-06-29 11:53:20 -0400

    scala:phone-number iteration 2/lazy vals

--- scala/phone-number/src/main/scala/phone_number.scala
+++ scala/phone-number/src/main/scala/phone_number.scala
@@ -2,16 +2,16 @@ class PhoneNumber(raw: String) {
 
   val DefaultNumber = "0000000000"
 
-  val filtered: String = {
-    raw.filter(_.isLetterOrDigit)
-  }
-
-  val number: String = {
+  lazy val number: String = {
     filtered match {
       case num if isValidTenDigits(num) => filtered
       case num if isValidElevenDigits(num) => filtered.tail
       case _ => DefaultNumber
     }
+  }
+
+  private lazy val filtered: String = {
+    raw.filter(_.isLetterOrDigit)
   }
 
   private def isValidTenDigits(num: String): Boolean = {
@@ -22,7 +22,7 @@ class PhoneNumber(raw: String) {
     num.head == '1' && num.length == 11
   }
 
-  val areaCode: String = {
+  lazy val areaCode: String = {
     number.substring(0, 3)
   }
 
