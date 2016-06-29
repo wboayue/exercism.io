a135b57bbf8d13d63afb71ddd5ab1a1050709020
Wil Boayue <wboayue@targetedvictory.com>
2016-06-29 12:16:36 -0400

    scala:phone-number iteration 3

--- scala/phone-number/src/main/scala/phone_number.scala
+++ scala/phone-number/src/main/scala/phone_number.scala
@@ -1,11 +1,11 @@
 class PhoneNumber(raw: String) {
 
-  val DefaultNumber = "0000000000"
+  val DefaultNumber = "0" * 10
 
   lazy val number: String = {
     filtered match {
-      case num if isValidTenDigits(num) => filtered
-      case num if isValidElevenDigits(num) => filtered.tail
+      case num if isValidTenDigits(num) => num
+      case num if isValidElevenDigits(num) => num.tail
       case _ => DefaultNumber
     }
   }
@@ -22,16 +22,14 @@ class PhoneNumber(raw: String) {
     num.head == '1' && num.length == 11
   }
 
-  lazy val areaCode: String = {
-    number.substring(0, 3)
-  }
+  lazy val areaCode: String = number.take(3)
+
+  private lazy val coCode: String = number.substring(3, 6)
+
+  private lazy val last4Digits: String = number.drop(6)
 
   override def toString: String = {
-    val npaCode = number.take(3)
-    val coCode = number.take(6).drop(3)
-    val digits = number.drop(6)
-
-    s"(${npaCode}) ${coCode}-${digits}"
+    s"(${areaCode}) ${coCode}-${last4Digits}"
   }
 
 }
