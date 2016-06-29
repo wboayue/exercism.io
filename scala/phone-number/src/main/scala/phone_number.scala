1edef927c377baab4da67aa8f3aa737e37889db6
Wil Boayue <wboayue@targetedvictory.com>
2016-06-29 14:09:19 -0400

    scala:phone-number cleanup

--- scala/phone-number/src/main/scala/phone_number.scala
+++ scala/phone-number/src/main/scala/phone_number.scala
@@ -1,12 +1,12 @@
 class PhoneNumber(raw: String) {
 
-  val DefaultNumber = "0" * 10
+  val InvalidNumber = "0" * 10
 
   lazy val number: String = {
     filtered match {
       case num if isValidTenDigits(num) => num
       case num if isValidElevenDigits(num) => num.tail
-      case _ => DefaultNumber
+      case _ => InvalidNumber
     }
   }
 
@@ -24,12 +24,12 @@ class PhoneNumber(raw: String) {
 
   lazy val areaCode: String = number.take(3)
 
-  private lazy val coCode: String = number.substring(3, 6)
+  private lazy val prefix: String = number.substring(3, 6)
 
-  private lazy val last4Digits: String = number.drop(6)
+  private lazy val lineNumber: String = number.drop(6)
 
   override def toString: String = {
-    s"(${areaCode}) ${coCode}-${last4Digits}"
+    s"(${areaCode}) ${prefix}-${lineNumber}"
   }
 
 }
