20ce4dbf69b230d4adfa73f924313ebc3ce8510e
Wil Boayue <wboayue@targetedvictory.com>
2016-06-30 15:39:29 -0400

    elixir:beer-song refactor

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -7,17 +7,26 @@ defmodule BeerSong do
     bottles_of_beer_lyrics(number) <> take_one_down_lyrics(number)
   end
 
-  def bottles_of_beer_lyrics(1) do
-    "No more bottles of beer on the wall, no more bottles of beer.\n"
+  defp pluralize(word, count, none \\ "no more") do
+    case count do
+      0 -> "#{none} #{word}s"
+      1 -> "1 #{word}"
+      _ -> "#{count} #{word}s"
+    end
   end
 
-  def bottles_of_beer_lyrics(2) do
-    "1 bottle of beer on the wall, 1 bottle of beer.\n"
+  def bottles_of_beer_lyrics(verse_number) do
+    bottle_count = verse_number - 1
+
+    sentence_case("#{beer_on_the_wall(bottle_count)}, #{pluralize("bottle", bottle_count)} of beer.\n")
   end
 
-  def bottles_of_beer_lyrics(number) do
-    count = number - 1
-    "#{count} bottles of beer on the wall, #{count} bottles of beer.\n"
+  def sentence_case(<< first_letter::binary-size(1) >> <> rest) do
+    String.upcase(first_letter) <> rest
+  end
+
+  def beer_on_the_wall(bottle_count) do
+    "#{pluralize("bottle", bottle_count)} of beer on the wall"
   end
 
   def take_one_down_lyrics(1) do
@@ -25,15 +34,11 @@ defmodule BeerSong do
   end
 
   def take_one_down_lyrics(2) do
-    "Take it down and pass it around, no more bottles of beer on the wall.\n"
-  end
-
-  def take_one_down_lyrics(3) do
-    "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    "Take it down and pass it around, #{beer_on_the_wall(0)}.\n"
   end
 
   def take_one_down_lyrics(number) do
-    "Take one down and pass it around, #{number - 2} bottles of beer on the wall.\n"
+    "Take one down and pass it around, #{beer_on_the_wall(number - 2)}.\n"
   end
 
   @doc """
