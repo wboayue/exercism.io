79ad9dd72b5618662c84a09b0eb07b876e99b573
Wil Boayue <wil.boayue@gmail.com>
2016-07-01 03:35:40 -0400

    scala:grade-school

---
+++ scala/grade-school/src/main/scala/grade_school.scala
@@ -0,0 +1,22 @@
+import scala.collection.immutable.TreeMap
+
+class School {
+
+  private var classes = TreeMap[Int, Seq[String]]()
+
+  def db = classes
+
+  def add(name: String, grade: Int): Unit = {
+    val students: Seq[String] = classes.getOrElse(grade, List[String]())
+    classes = classes + (grade -> (students :+ name))
+  }
+
+  def grade(number: Int): Seq[String] = {
+    classes.getOrElse(number, Seq.empty)
+  }
+
+  def sorted(): Map[Int, Seq[String]] = {
+    classes.mapValues(_.sorted)
+  } 
+
+}
