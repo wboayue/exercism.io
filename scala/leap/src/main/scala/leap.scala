c8d81350e9a00705571c9e2557471e03d29278ff
Wil Boayue <wboayue@targetedvictory.com>
2016-07-08 10:20:59 -0400

    scala:leap

--- scala/leap/src/main/scala/leap.scala
+++ scala/leap/src/main/scala/leap.scala
@@ -1,14 +1,12 @@
 class Year(year: Int) {
 
-  implicit class IntUtils(val i: Int) {
-    def isDivisibleBy(factor: Int): Boolean = i % factor == 0 
+  implicit class IntHelpers(val i: Int) {
+    def isDivisibleBy(factor: Int): Boolean = i % factor == 0
   }
 
-  def isLeap: Boolean = year match {
-    case year if year isDivisibleBy 400 => true
-    case year if year isDivisibleBy 100 => false
-    case year if year isDivisibleBy 4 => true
-    case _ => false
+  def isLeap: Boolean = {
+    year.isDivisibleBy(400) ||
+      (year.isDivisibleBy(4) && !year.isDivisibleBy(100))
   }
 
 }
