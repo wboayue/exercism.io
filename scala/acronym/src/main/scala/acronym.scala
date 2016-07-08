c6e30947e30e55b0cb985c0bfe1b3f74d730a7c5
Wil Boayue <wboayue@targetedvictory.com>
2016-07-08 10:44:50 -0400

    scala:acromyn

--- scala/acronym/src/main/scala/acronym.scala
+++ scala/acronym/src/main/scala/acronym.scala
@@ -1,19 +1,29 @@
-case class Acronym(phrase: String) {
+class Acronym(phrase: String) {
 
   val abbreviate: String = {
     val (acronym, previous) = phrase.foldLeft(("", ' ')) {
-      (acc: (String, Char), current: Char) =>
-        acc match {
-          case (acronym, previous) if isBreakPoint(current, previous) => (acronym + current, current)
-          case (acronym, _) => (acronym, current)
+      (accumulator: (String, Char), current: Char) =>
+        accumulator match {
+          case (acronym, previous) if Acronym.isBreakPoint(current, previous)
+            => (acronym + Character.toUpperCase(current), current)
+          case (acronym, _)
+            => (acronym, current)
         }
     }
 
-    acronym.toUpperCase
+    acronym
   }
 
-  private def isBreakPoint(current: Char, previous: Char): Boolean = {
-    Set(' ', '-').contains(previous) || (current.isUpper && previous.isLower)
+}
+
+object Acronym {
+
+  val Separators = Set(' ', '-')
+
+  def apply(phrase: String) = new Acronym(phrase)
+
+  def isBreakPoint(current: Char, previous: Char): Boolean = {
+    Separators.contains(previous) || (current.isUpper && previous.isLower)
   }
 
 }
