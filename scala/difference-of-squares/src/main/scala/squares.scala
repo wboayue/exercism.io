16bd34d9fce65b5af2a73e848ca595334bb998d5
Wil Boayue <wil.boayue@gmail.com>
2016-07-09 16:04:40 -0400

    scala:difference-of-squares

--- scala/difference-of-squares/src/main/scala/squares.scala
+++ scala/difference-of-squares/src/main/scala/squares.scala
@@ -3,11 +3,11 @@ object Squares {
   def apply() = this
 
   def squareOfSums(n: Int): Double = {
-    Math.pow((n * (n + 1) / 2), 2)
+    math.pow((n * (n + 1) / 2), 2)
   }
 
   def sumOfSquares(n: Int): Double = {
-    (2 * Math.pow(n, 3) + 3 * Math.pow(n, 2) + n) / 6
+    (2 * math.pow(n, 3) + 3 * math.pow(n, 2) + n) / 6
   }
 
   def difference(n: Int): Double = {
