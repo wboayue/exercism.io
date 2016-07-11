82d451a754a552317de6a8e48636e80673f19d4a
Wil Boayue <wboayue@targetedvictory.com>
2016-07-11 11:41:51 -0400

    scala:space-age

--- scala/space-age/src/main/scala/space_age.scala
+++ scala/space-age/src/main/scala/space_age.scala
@@ -1,26 +1,23 @@
 import scala.math.BigDecimal.RoundingMode
 
 class SpaceAge(val seconds: BigDecimal) {
+  import SpaceAge.SpaceAgeBigDecimal
 
-  lazy val onEarth = SpaceAge.ageOn(seconds, Planet.Earth)
-  lazy val onMercury = SpaceAge.ageOn(seconds, Planet.Mercury)
-  lazy val onVenus = SpaceAge.ageOn(seconds, Planet.Venus)
-  lazy val onMars = SpaceAge.ageOn(seconds, Planet.Mars)
-  lazy val onJupiter = SpaceAge.ageOn(seconds, Planet.Jupiter)
-  lazy val onSaturn = SpaceAge.ageOn(seconds, Planet.Saturn)
-  lazy val onUranus = SpaceAge.ageOn(seconds, Planet.Uranus)
-  lazy val onNeptune = SpaceAge.ageOn(seconds, Planet.Neptune)
-
+  lazy val onEarth = seconds.toYears(Planet.Earth)
+  lazy val onMercury = seconds.toYears(Planet.Mercury)
+  lazy val onVenus = seconds.toYears(Planet.Venus)
+  lazy val onMars = seconds.toYears(Planet.Mars)
+  lazy val onJupiter = seconds.toYears(Planet.Jupiter)
+  lazy val onSaturn = seconds.toYears(Planet.Saturn)
+  lazy val onUranus = seconds.toYears(Planet.Uranus)
+  lazy val onNeptune = seconds.toYears(Planet.Neptune)
 }
 
 sealed class Planet(coefficient: Double) {
-
   val orbitalPeriod: BigDecimal  = SpaceAge.EarthOrbitalPeriod * coefficient
-
 }
 
 object Planet {
-
   val Earth = new Planet(1.0)
   val Mercury = new Planet(0.2408467)
   val Venus = new Planet(0.61519726)
@@ -29,16 +26,16 @@ object Planet {
   val Saturn = new Planet(29.447498)
   val Uranus = new Planet(84.016846)
   val Neptune = new Planet(164.79132)
-
 }
 
 object SpaceAge {
-
   val EarthOrbitalPeriod: BigDecimal = 31557600
 
   def apply(seconds: Double) = new SpaceAge(seconds)
 
-  def ageOn(seconds: BigDecimal, planet: Planet): BigDecimal = {
-    (seconds / planet.orbitalPeriod).setScale(2, RoundingMode.HALF_UP)
+  implicit class SpaceAgeBigDecimal(seconds: BigDecimal) {
+    def toYears(planet: Planet): BigDecimal = {
+      (seconds / planet.orbitalPeriod).setScale(2, RoundingMode.HALF_UP)
+    }
   }
 }
