97aa580ebb6a5f8d87596a6e677a5fd2a9cc090b
Wil Boayue <wil.boayue@gmail.com>
2016-07-12 02:03:42 -0400

    scala:grains

---
+++ scala/grains/src/main/scala/grains.scala
@@ -0,0 +1,14 @@
+// This exercise describes the geometric series derived from flipping
+// the bits of a 64 bit binary number.
+// http://www.regentsprep.org/regents/math/algtrig/atp2/geoseq.htm
+object Grains {
+
+  val CommonRatio = BigInt(2)
+
+  def square(n: Int): BigInt = {
+    CommonRatio.pow(n - 1)
+  }
+
+  lazy val total = (1 - CommonRatio.pow(64)) / (1 - CommonRatio)
+  
+}
