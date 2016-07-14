e08e9e22bb0a291ee83d4bff1641fdb5d510328e
Wil Boayue <wboayue@targetedvictory.com>
2016-07-14 12:47:47 -0400

    scala:accumulate

--- scala/accumulate/src/main/scala/accumulate.scala
+++ scala/accumulate/src/main/scala/accumulate.scala
@@ -1,7 +1,7 @@
 class Accumulate() {
 
   def accumulate[X, Y](mapper: (X) => Y, items: List[X]): List[Y] = {
-    items.foldRight(List[Y]()) {
+    items.foldRight(List.empty[Y]) {
       case (item, results) => mapper(item) :: results
     }
   }
