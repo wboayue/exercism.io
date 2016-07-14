7ac587d200ad37ba5a4b549a3c19f9e1ea099d26
Wil Boayue <wboayue@targetedvictory.com>
2016-07-14 12:43:33 -0400

    scala:accumulate

--- scala/accumulate/src/main/scala/accumulate.scala
+++ scala/accumulate/src/main/scala/accumulate.scala
@@ -1,16 +1,8 @@
-import scala.annotation.tailrec
-
 class Accumulate() {
 
   def accumulate[X, Y](mapper: (X) => Y, items: List[X]): List[Y] = {
-    accumulate(mapper, items, Nil)
-  }
-
-  @tailrec
-  private def accumulate[X, Y](mapper: (X) => Y, items: List[X], accumulator: List[Y]): List[Y] = {
-    items match {
-      case Nil => accumulator.reverse
-      case head :: tail => accumulate(mapper, tail, mapper(head) :: accumulator)
+    items.foldRight(List[Y]()) {
+      case (item, results) => mapper(item) :: results
     }
   }
 
