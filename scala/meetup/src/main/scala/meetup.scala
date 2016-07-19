b31b1e23ccff0a1ff988da25b781a47cda9b999f
Wil Boayue <wboayue@targetedvictory.com>
2016-07-19 15:58:07 -0400

    scala:meetup

--- scala/meetup/src/main/scala/meetup.scala
+++ scala/meetup/src/main/scala/meetup.scala
@@ -4,105 +4,59 @@ class Meetup(month: Int, year: Int) {
   import Meetup._
 
   def first(weekday: WeekDay): Calendar = {
-    startOfMonth(month, year) next weekday
+    WeekDaySeq(month, year, weekday).head
   }
 
   def second(weekday: WeekDay): Calendar = {
-    1 weekFrom first(weekday)
+    WeekDaySeq(month, year, weekday).drop(1).head
   }
 
   def third(weekday: WeekDay): Calendar = {
-    2 weeksFrom first(weekday)
+    WeekDaySeq(month, year, weekday).drop(2).head
   }
 
   def fourth(weekday: WeekDay): Calendar = {
-    3 weeksFrom first(weekday)
+    WeekDaySeq(month, year, weekday).drop(3).head
   }
 
   def teenth(weekday: WeekDay): Calendar = {
-    startOfMonth(month, year) teenth weekday
+    WeekDaySeq(month, year, weekday)
+      .filter(Meetup.Teenths contains _.get(Calendar.DATE))
+      .head
   }
 
   def last(weekday: WeekDay): Calendar = {
-    endOfMonth(month, year) last weekday
+    WeekDaySeq(month, year, weekday).last
+  }
+
+}
+
+object WeekDaySeq {
+  import Meetup.WeekDay
+
+  def apply(month: Int, year: Int, weekday: WeekDay): Seq[Calendar] = {
+    val startOfMonth = new GregorianCalendar(year, month - 1, 1)
+    val endOfMonth = new GregorianCalendar(year, month, 1)
+
+    var weekdays = Array.empty[Calendar]
+
+    while (startOfMonth != endOfMonth) {
+      if (startOfMonth.get(Calendar.DAY_OF_WEEK) == weekday.day) {
+        weekdays = weekdays :+ startOfMonth.clone().asInstanceOf[Calendar]
+      }
+      startOfMonth.add(Calendar.DATE, 1)
+    }
+
+    weekdays
   }
 
 }
 
 object Meetup {
 
+  val Teenths = (13 to 19).toSet
+
   def apply(month: Int, year: Int) = new Meetup(month, year)
-
-  def startOfMonth(month: Int, year: Int): Calendar = {
-    new GregorianCalendar(year, month - 1, 1)
-  }
-
-  def endOfMonth(month: Int, year: Int): Calendar = {
-    new GregorianCalendar(year, month, 1) addDays -1
-  }
-
-  implicit class MeetupCalendar(date: Calendar) {
-
-    val Teenths = 13 to 19
-
-    def addDays(num: Int) = {
-      val copy = date.clone().asInstanceOf[Calendar]
-      copy.add(Calendar.DATE, num)
-      copy
-    }
-
-    def subtractDays(num: Int) = {
-      addDays(num * -1)
-    }
-
-    def setDayOfMonth(num: Int) = {
-      val copy = date.clone().asInstanceOf[Calendar]
-      copy.set(Calendar.DATE, num)
-      copy
-    }
-
-    def dayOfWeek() = {
-      date.get(Calendar.DAY_OF_WEEK)
-    }
-
-    def next(weekday: WeekDay): Calendar = {
-      val offset = weekday.day - date.dayOfWeek
-
-      if (offset < 0) {
-        date addDays 7 + offset
-      } else {
-        date addDays offset
-      }
-    }
-
-    def last(weekday: WeekDay): Calendar = {
-      val offset = date.dayOfWeek - weekday.day
-
-      if (offset < 0) {
-        date subtractDays (7 + offset)
-      } else {
-        date subtractDays offset
-      }
-    }
-
-    def teenth(weekday: WeekDay): Calendar = {
-      Teenths.map(date.setDayOfMonth(_))
-        .find(_.dayOfWeek == weekday.day).get
-    }
-
-  }
-
-  implicit class MeetupInt(num: Int) {
-
-    def weeksFrom(date: Calendar): Calendar = {
-      date addDays num * 7
-    }
-
-    def weekFrom(date: Calendar): Calendar = {
-      weeksFrom(date)
-    }
-
-  }
 
   sealed abstract class WeekDay(val day: Int)
   case object Mon extends WeekDay(Calendar.MONDAY)
