94a84236520d87a4692979718555a05f796d71ef
Wil Boayue <wil.boayue@gmail.com>
2016-07-22 08:26:28 -0400

    scala:rna-transctiption

---
+++ scala/rna-transcription/src/main/scala/transcription.scala
@@ -0,0 +1,16 @@
+object Dna {
+
+  def apply() = this
+
+  val DnaRna = Map[Char, Char](
+    'G' -> 'C',
+    'C' -> 'G',
+    'T' -> 'A',
+    'A' -> 'U'
+  )
+
+  def toRna(dna: String): String = {
+    dna.map(DnaRna(_))
+  }
+
+}
