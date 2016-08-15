3959217e863fa0ebd12c05d1439e070e16456db8
Wil Boayue <wil.boayue@gmail.com>
2016-08-15 01:00:19 -0400

    elixir:sum-of-multiples - use filter

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -4,19 +4,12 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
-    Enum.flat_map(factors, &(multiples(limit, &1)))
-    |> Enum.uniq
+    1..(limit - 1)
+    |> Enum.filter(&(multiple?(&1, factors)))
     |> Enum.sum
   end
 
-  defp multiples(limit, factors), do: multiples(limit, factors, [])
-  defp multiples(limit, factor, []) when limit < factor, do: []
-  defp multiples(limit, factor, []), do: multiples(limit, factor, [factor])
-  defp multiples(limit, factor, items) do
-    cond do
-      hd(items) + factor < limit -> multiples(limit, factor, [hd(items) + factor | items])
-      true -> items
-    end
+  defp multiple?(num, factors)  do
+    Enum.any?(factors, &(rem(num, &1) == 0))    
   end
-
 end
