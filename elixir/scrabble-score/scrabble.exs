1127d67affe358639e8699dd89903a551456570f
Wil Boayue <wil.boayue@gmail.com>
2016-08-14 23:06:13 -0400

    elixir:scrabble score

---
+++ elixir/scrabble-score/scrabble.exs
@@ -0,0 +1,27 @@
+defmodule Scrabble do
+
+  @raw_letter_values [
+    {"aeioulnrst",  1},
+    {"dg", 2},
+    {"bcmp", 3},
+    {"fhvwy", 4},
+    {"k", 5},
+    {"jx", 8},
+    {"qz", 10},
+  ]
+
+  @letter_values for {letters, score} <- @raw_letter_values, letter <- String.codepoints(letters), do: {letter, score}, into: %{}
+
+  @doc """
+  Calculate the scrabble score for the word.
+  """
+  @spec score(String.t) :: non_neg_integer
+  def score(word) do
+    word
+    |> String.downcase
+    |> String.replace(~r/[^a-z]/, "")
+    |> String.codepoints
+    |> Enum.map(&(@letter_values[&1]))
+    |> Enum.sum
+  end
+end
