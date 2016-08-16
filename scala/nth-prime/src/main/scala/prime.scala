624b5b27f519598a1f50238efd8e04d2c1c5ad13
Wil Boayue <wboayue@targetedvictory.com>
2016-08-16 11:16:38 -0400

    scala:nth-prime

---
+++ scala/nth-prime/src/main/scala/prime.scala
@@ -0,0 +1,29 @@
+import scala.collection.mutable.ArrayBuffer
+
+object Prime {
+
+  def nth(count: Int) = {
+    require(count > 0, "count must be greater than zero")
+    primes.apply(count - 1)
+  }
+
+  def isFactor(num: Int, factor: Int): Boolean = num % factor == 0
+
+  def primes(): Stream[Int] = {
+    var candidate: Int = 2
+    val knownPrimes = ArrayBuffer.empty[Int]
+
+    def nextPrime: Int = {
+      if (knownPrimes.exists(isFactor(candidate, _))) {
+        candidate += 1
+        nextPrime
+      } else {
+        knownPrimes.append(candidate)
+        candidate
+      }
+    }
+
+    Stream.continually(nextPrime)
+  }
+
+}
