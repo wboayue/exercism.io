740c126d3fa696eddcfc750eee9f2cd9a4cf6122
Wil Boayue <wil.boayue@gmail.com>
2016-08-29 00:25:21 -0400

    java:robot-name

---
+++ java/robot-name/src/main/java/Robot.java
@@ -0,0 +1,59 @@
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+class Robot {
+  
+  private static final RandomNames names = new RandomNames();
+  private String name;
+
+  public Robot() {
+    reset();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void reset() {
+    this.name = this.names.next();
+  }
+
+  private static class RandomNames implements Iterator<String> {
+
+    private Random random = new Random();
+    private Set<String> generatedNames = new TreeSet<String> ();
+    private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public String next() {
+      String nextName;
+
+      do {
+        nextName = String.format("%s%s", letters(), digits());
+      } while (!generatedNames.add(nextName));
+
+      return nextName;
+    }
+
+    public boolean hasNext() {
+      return true;
+    }
+
+    private String digits() {
+      return this.random.ints(0, 10)
+        .limit(3)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining());
+    }
+
+    private String letters() {
+      return this.random.ints(0, 26)
+        .limit(2)
+        .mapToObj(i -> String.valueOf(LETTERS.charAt(i)))
+        .collect(Collectors.joining());
+    }
+
+  }
+}
