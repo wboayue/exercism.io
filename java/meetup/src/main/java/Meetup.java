f7db172aa893527585163cf812097d261c7e7522
Wil Boayue <wboayue@targetedvictory.com>
2016-08-29 16:23:33 -0400

    java:meetup

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -38,15 +38,15 @@ public class Meetup {
         return days.get(3);
       case LAST:
         return days.get(days.size() - 1);
+      case TEENTH:
+        List<LocalDate> teenth = Meetup.findTeenth(days);
+        if (teenth.isEmpty()) {
+          throw new IllegalArgumentException(String.format("Teenth not found in: %s", days));
+        }
+        return teenth.get(0);
       default:
-        throw new IllegalArgumentException(String.format("Unsupport option: %s", schedule));
+        throw new IllegalArgumentException(String.format("Unsupported option: %s", schedule));
     }
-  }
-
-  private static List<LocalDate> findDays(int year, int month, DayOfWeek dayOfWeek) {
-    return Meetup.daysInMonth(year, month)
-      .filter(day -> day.getDayOfWeek() == dayOfWeek)
-      .collect(Collectors.toList());
   }
 
   private static Stream<LocalDate> daysInMonth(int year, int month) {
@@ -59,6 +59,18 @@ public class Meetup {
     }
 
     return matches.stream();
+  }
+
+  private static List<LocalDate> findDays(int year, int month, DayOfWeek dayOfWeek) {
+    return Meetup.daysInMonth(year, month)
+      .filter(day -> day.getDayOfWeek() == dayOfWeek)
+      .collect(Collectors.toList());
+  }
+
+  private static List<LocalDate> findTeenth(List<LocalDate> days) {
+    return days.stream()
+      .filter(day -> day.getDayOfMonth() >= 13 && day.getDayOfMonth() <= 19)
+      .collect(Collectors.toList());
   }
 
 }
