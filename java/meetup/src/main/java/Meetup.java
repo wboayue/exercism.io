eae30999d377a2c155711cc625d1effcfa9dc62e
Wil Boayue <wboayue@targetedvictory.com>
2016-08-29 16:26:00 -0400

    java:meetup

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,7 +1,5 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.ZoneId;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
