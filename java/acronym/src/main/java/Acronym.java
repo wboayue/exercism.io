a33a8e458b16fa4d05992a42bfb0e52732674ef9
Wil Boayue <wboayue@targetedvictory.com>
2016-09-01 00:25:56 -0400

    java:acronym

--- java/acronym/src/main/java/Acronym.java
+++ java/acronym/src/main/java/Acronym.java
@@ -1,28 +1,20 @@
-import java.util.function.IntPredicate;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Acronym {
 
+  private static final Pattern SPLIT_PATTERN = Pattern.compile("(\\W+|[a-z](?=[A-Z]))");
+
   public static String generate(String phrase) {
-    class LetterFilter implements IntPredicate {
-      private int previous = ' ';
+    return SPLIT_PATTERN
+      .splitAsStream(phrase)
+      .map(Acronym::extractSignificantLetter)
+      .collect(Collectors.joining(""));
+  }
 
-      @Override
-      public boolean test(int current) {
-        int lastCodePoint = previous;
-        this.previous = current;
-
-        return (Character.isLetter(current) && !Character.isLetterOrDigit(lastCodePoint))
-          || (Character.isUpperCase(current) && Character.isLowerCase(lastCodePoint));
-      }
-    }
-
-    LetterFilter isPartOfAcronym = new LetterFilter();
-
-    return phrase.codePoints()
-      .filter(isPartOfAcronym)
-      .map(Character::toUpperCase)
-      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-      .toString();
+  private static final String extractSignificantLetter(String word) {
+    return word.toUpperCase().substring(0, 1);
   }
 
 }
