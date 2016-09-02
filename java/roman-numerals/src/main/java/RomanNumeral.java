b8178ba10d593280368691cde0165de2d486293a
Wil Boayue <wboayue@targetedvictory.com>
2016-09-02 00:17:57 -0400

    checkpoint

---
+++ java/roman-numerals/src/main/java/RomanNumeral.java
@@ -0,0 +1,53 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class RomanNumeral {
+
+  private int number;
+  private static Map<Integer, String> NUMBER_MAP = buildNumberMap();
+
+  public RomanNumeral(int number) {
+    this.number = number;
+  }
+
+  public String getRomanNumeral() {
+    StringBuilder translated = new StringBuilder();
+
+    int remaining = this.number;
+    for (Map.Entry<Integer, String> entry : NUMBER_MAP) {
+      int abrabic = entry.getKey();
+      String roman = entry.getValue();
+
+      translated.append();
+    }
+
+    return translated.toString();
+
+
+      (numerals + (roman * (number / abrabic)), number % abrabic)
+    }
+
+    translated
+
+  }
+
+  private static Map<Integer, String> buildNumberMap() {
+    Map<Integer, String> mapping = new TreeMap<>(Comparator.reverseOrder());
+    mapping.put(1000, "M");
+    mapping.put(900, "CM");
+    mapping.put(500, "D");
+    mapping.put(400, "CD");
+    mapping.put(100, "C");
+    mapping.put(90, "XC");
+    mapping.put(50, "L");
+    mapping.put(40, "XL");
+    mapping.put(10, "X");
+    mapping.put(9, "IX");
+    mapping.put(5, "V");
+    mapping.put(4, "IV");
+    mapping.put(1, "I");
+    return mapping;
+  }
+
+}
