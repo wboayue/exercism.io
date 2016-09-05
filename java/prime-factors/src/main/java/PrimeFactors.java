7c61e678a344cc45a341d711961617fca39fe086
Wil Boayue <wil.boayue@gmail.com>
2016-09-05 17:14:31 -0400

    java:prime-factors

--- java/prime-factors/src/main/java/PrimeFactors.java
+++ java/prime-factors/src/main/java/PrimeFactors.java
@@ -1,58 +1,24 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Collections;
 
 public class PrimeFactors {
 
   public static List<Long> getForNumber(long number) {
-    List<Long> factors = new ArrayList();
-    Primes primes = new Primes();
-    factors.add(primes.next());
-    factors.add(primes.next());
-    factors.add(primes.next());
-    factors.add(primes.next());
-    factors.add(primes.next());
-    factors.add(primes.next());
-    factors.add(primes.next());
-    factors.add(primes.next());
-    return factors;
-  }
+    List<Long> primes = new ArrayList();
+    
+    long factor = 2;
+    long remainder = number;
 
-  private static class Primes implements Iterator<Long> {
-
-    private List<Long> knownPrimes = new ArrayList<>();
-    private Long candidate = 2L;
-
-    public Long next() {
-      while (!isPrime(this.candidate)) {
-        if (this.candidate == 2L) {
-          this.candidate += 1L;
-        } else {
-          this.candidate += 2L;
-        }
-        break;
+    while (remainder != 1 && factor <= number) {
+      if (remainder % factor == 0) {
+        remainder /= factor;
+        primes.add(factor);
+      } else {
+        factor += 1;
       }
-
-      this.knownPrimes.add(candidate);
-      this.candidate += 2L;
-
-      return candidate;
     }
 
-    private boolean isPrime(long number) {
-      // can be more efficient by only checking to up sqrt(number)
-      for (long prime : this.knownPrimes) {
-        if (number % prime == 0) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    public boolean hasNext() {
-      return true;
-    }
+    return primes;
   }
 
 }
