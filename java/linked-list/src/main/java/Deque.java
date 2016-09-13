2e189d3419d5f44490aace522e4fc94776033c4f
Wil Boayue <wil.boayue@gmail.com>
2016-09-12 21:41:48 -0400

    java:linked-list

--- java/linked-list/src/main/java/Deque.java
+++ java/linked-list/src/main/java/Deque.java
@@ -2,79 +2,79 @@ import java.util.NoSuchElementException;
 
 public class Deque<T> {
 
-  private Node front = null;
-  private Node back = null;
+  private Node<T> front = null;
+  private Node<T> back = null;
+
+  public boolean isEmpty() {
+    return this.front == null || this.back == null;
+  }
 
   public Deque push(T item) {
-    this.front = new Node(item, null, this.front);
-
-    if (this.front.next != null) {
-      this.front.next.previous = this.front;
+    if (isEmpty()) {
+      addFirstItem(item);
+    } else {
+      this.front = this.front.addItemAhead(item);
     }
-
-    if (this.back == null) {
-      this.back = this.front;
-    }
-
     return this;
   }
 
+  private void addFirstItem(T item) {
+    this.front = this.back = new Node(item);
+  }
+
   public T pop() {
-    if (this.front == null) {
-      throw new NoSuchElementException();
-    }
-
-    Node<T> result = this.front;
-
+    if (isEmpty()) throw new NoSuchElementException();
+  
+    T item = this.front.item;
     this.front = this.front.next;
-
-    if (this.front == null) {
-      this.back = null;
-    }
-
-    return result.item;
+    if (this.front != null) this.front.previous = null;
+    return item;
   }
 
   public T shift() {
-    if (this.back == null) {
-      throw new NoSuchElementException();
-    }
+    if (isEmpty()) throw new NoSuchElementException();
 
-    Node<T> result = this.back;
-
+    T item = this.back.item;
     this.back = this.back.previous;
-
-    if (this.back == null) {
-      this.front = null;
-    }
-
-    return result.item;
+    if (this.back != null) this.back.next = null;
+    return item;
   }
 
   public Deque unshift(T item) {
-    this.back = new Node(item, this.back, null);
-
-    if (this.back.previous != null) {
-      this.back.previous.next = this.back;
+    if (isEmpty()) {
+      addFirstItem(item);
+    } else {
+      this.back = this.back.addItemBehind(item);
     }
-
-    if (this.front == null) {
-      this.front = this.back;
-    }
-
     return this;    
   }
 
   private static class Node<T> {
     
     private T item;
-    private Node previous;
-    private Node next;
+    private Node previous = null;
+    private Node next = null;
 
-    public Node(T item, Node previous, Node next) {
+    private Node(T item) {
+      this.item = item;
+    }
+
+    private Node(T item, Node previous, Node next) {
       this.item = item;
       this.previous = previous;
       this.next = next;
+    }
+
+    private Node addItemAhead(T item) {
+      Node inserted = new Node(item, null, this);
+      this.previous = inserted;
+      return inserted;
+    }
+
+    private Node addItemBehind(T item) {
+      Node inserted = new Node(item, this, null);
+      this.next = inserted;
+      return inserted;
     }
 
   }
