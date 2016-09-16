92a57bc2ab86955e8e5d1640691ea8f8089c1314
Wil Boayue <wboayue@targetedvictory.com>
2016-09-16 17:43:32 -0400

    scala:pascals-triangle

--- scala/pascals-triangle/src/main/scala/PascalsTriangle.scala
+++ scala/pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -3,15 +3,13 @@ object PascalsTriangle {
   def triangle(num: Int): List[List[Int]] = {
     require(num > 0, "rows must be greater than 0")
     Stream.iterate(List(1))(buildRow(_))
-      .take(num)
-      .toList
+      .take(num).toList
   }
 
   private def buildRow(previous: List[Int]): List[Int] = {
     (0 :: previous ++ List(0))
       .sliding(2,1)
-      .map(_.sum)
-      .toList
+      .map(_.sum).toList
   }
 
 }
