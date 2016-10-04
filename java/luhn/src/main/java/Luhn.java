bc9f8cbd99749570028da2f434838c76370a681f
Wil Boayue <wboayue@targetedvictory.com>
2016-10-04 00:47:40 -0400

    java:luhn refactor

--- java/luhn/src/main/java/Luhn.java
+++ java/luhn/src/main/java/Luhn.java
@@ -7,7 +7,11 @@ public class Luhn {
   private int[] digits;
 
   public Luhn(long number) {
-    this.digits = String.valueOf(number).chars().map(d -> d - '0').toArray();
+    this.digits = toDigits(number);
+  }
+
+  public static int[] toDigits(long number) {
+    return String.valueOf(number).chars().map(d -> d - '0').toArray();
   }
 
   public int getCheckDigit() {
@@ -16,12 +20,15 @@ public class Luhn {
 
   public int[] getAddends() {
     int[] addends = new int[this.digits.length];
+
     for (int i = addends.length - 1, j = 0; i >= 0; --i, ++j) {
       addends[i] = j % 2 == 0 ?  this.digits[i] : this.digits[i] * 2;
+
       if (addends[i] > 9) {
         addends[i] = addends[i] - 9;
       }
     }
+
     return addends;
   }
 
@@ -34,9 +41,10 @@ public class Luhn {
   }
 
   public static long create(long number) {
-    Luhn luhn = new Luhn(number * 10);
-    int diff = luhn.getCheckSum() % 10;
-    return number * 10 + ((10 - diff) % 10);
+    long numberWithPlaceholder = number * 10;
+    Luhn luhn = new Luhn(numberWithPlaceholder);
+    int checkDigit = (10 - (luhn.getCheckSum() % 10)) % 10;
+    return numberWithPlaceholder + checkDigit;
   }
 
 }
