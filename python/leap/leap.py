9f05cb1d100e610bb296f39efa9c508beb6711d7
Wil Boayue <wil.boayue@gmail.com>
2016-10-25 22:48:17 -0400

    leap:python

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,5 +1,6 @@
 def is_leap_year(year):
-  return divisible_by(year, 400) or (divisible_by(year, 4) and not divisible_by(year, 100))
 
-def divisible_by(numerator, denominator):
-  return numerator % denominator == 0
+  def divisible_by(x, y):
+    return x % y == 0
+
+  return divisible_by(year, 400) or (divisible_by(year, 4) and not divisible_by(year, 100))
