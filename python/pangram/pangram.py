3eb0f3ac77749d032c68a83c8a6607cdd9da698e
Wil Boayue <wil.boayue@gmail.com>
2016-10-26 00:23:19 -0400

    python:pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import string
 
-alphabet = set([x for x in string.ascii_lowercase])
+alphabet = set(string.ascii_lowercase)
 
 def is_pangram(sentence):
-  sentence = sentence.lower()
-  letters = set([x for x in sentence])
-  return alphabet.issubset(letters)
+  return alphabet.issubset(set(sentence.lower()))
