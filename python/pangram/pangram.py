7918ed6017913e70d0a90b22c17ba55705e8264b
Wil Boayue <wil.boayue@gmail.com>
2016-10-26 00:15:16 -0400

    python:pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -4,6 +4,5 @@ alphabet = set([chr(x) for x in xrange(ord('a'), ord('z'))])
 
 def is_pangram(sentence):
   sentence = sentence.lower()
-  letters = [sentence[i] for i in xrange(len(sentence))]
+  letters = set([sentence[i] for i in xrange(len(sentence))])
   return alphabet.issubset(letters)
-  
