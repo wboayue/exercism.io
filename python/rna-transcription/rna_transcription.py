e74aba3227f28af0057e0ab54e00195b6dfa71ce
Wil Boayue <wboayue@targetedvictory.com>
2016-10-26 12:45:31 -0400

    rna-transcription:python

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-
-dna_to_rna = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+from string import maketrans
 
 def to_rna(dna_strand):
-  return ''.join([dna_to_rna[nucleotide] for nucleotide in dna_strand])
+  return dna_strand.translate(maketrans('GCTA', 'CGAU'))
