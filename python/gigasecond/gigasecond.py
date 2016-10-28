c74ced75173a749829513f549816d091ffd5cc33
Wil Boayue <wboayue@targetedvictory.com>
2016-10-28 16:54:05 -0400

    gigasecond:python

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 
-GIGA_SECOND = timedelta(seconds=10**9)
-
 def add_gigasecond(date_time):
-  return date_time + GIGA_SECOND
+  return date_time + timedelta(seconds=10**9)
