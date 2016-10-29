b5b30a72c04b4e31541cd103d375e4613d10acc5
Wil Boayue <wil.boayue@gmail.com>
2016-10-29 00:49:36 -0400

    python:run-length-encoding decode

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -2,17 +2,39 @@
 from itertools import groupby
 
 def encode(plain):
-  return ''.join([encode_chunk(chunk) for chunk in chunks(plain)])
+  return ''.join([encode_chunk(*chunk) for chunk in chunks(plain)])
 
 def chunks(plain):
   return [(run_type, len(list(run))) for run_type, run in groupby(plain)]
 
-def encode_chunk(run):
-  (run_type, run_length) = run
+def encode_chunk(run_type, run_length):
   if run_length == 1:
     return run_type
   else:
     return u"{0}{1}".format(run_length, run_type)
 
 def decode(compressed):
-  return compressed
+  chunks = [decode_chunk(*chunk) for chunk in groupby(compressed, lambda x: x.isdigit())]
+  plain = ''
+  run_length = None
+  for token in chunks:
+    if isinstance(token, (long, int)):
+      run_length = token
+    else:
+      if run_length:
+        plain += token[0] * run_length
+        if len(token) > 1:
+          plain += token[1:]
+        run_length = None
+      else:
+        plain += token
+
+  return plain
+
+def decode_chunk(is_digit, chunk):
+  chunk = ''.join(list(chunk))
+  if is_digit:
+    return int(chunk)
+  else:
+    return chunk
+ 
