fb36e37b259e41ef5f6696aaa724eae1408f2931
Wil Boayue <wboayue@targetedvictory.com>
2016-10-31 01:05:29 -0400

    java:beer-song

---
+++ java/beer-song/src/main/java/BeerSong.java
@@ -0,0 +1,15 @@
+public class BeerSong {
+
+  public static String verse(int i) {
+    return "";
+  }
+
+  public static String sing(int start, int end) {
+    return "";
+  }
+
+  public static String singSong() {
+    return "";
+  }
+
+}
