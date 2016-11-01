df1a2be2fa130da56919aa4e6d970cb558205e96
Wil Boayue <wil.boayue@gmail.com>
2016-11-01 00:26:46 -0400

    java:beer-song

--- java/beer-song/src/main/java/BeerSong.java
+++ java/beer-song/src/main/java/BeerSong.java
@@ -1,15 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeerSong {
 
-  public static String verse(int i) {
-    return "";
+  public static String verse(int number) {
+    return singBottlesOfBeer(number) + singTakeOneDown(number);
+  }
+
+  private static String singBottlesOfBeer(int number) {
+    return capitalize(singBeerOnWall(number) + ", " + pluralize("bottle", number) + " of beer.\n");
+  }
+
+  private static String singBeerOnWall(int count) {
+    return String.format("%s of beer on the wall", pluralize("bottle", count));
   }
 
   public static String sing(int start, int end) {
-    return "";
+    List<String> verses = new ArrayList<>();
+
+    for (int verseNumber = start; verseNumber >= end; --verseNumber) {
+      verses.add(verse(verseNumber));
+    }
+
+    return String.join("", verses);
   }
 
   public static String singSong() {
-    return "";
+    return sing(99, 0);
+  }
+
+  private static String singTakeOneDown(int number) {
+    switch (number) {
+      case 0:
+        return "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+      case 1:
+        return "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
+      default:
+        return String.format("Take one down and pass it around, %s.\n\n", singBeerOnWall(number - 1));
+    }
+  }
+
+  private static String capitalize(String sentence) {
+    return sentence.substring(0, 1).toUpperCase() + sentence.substring(1);
+  }
+
+  private static String pluralize(String word, int count) {
+    switch (count) {
+      case 0:
+        return String.format("no more %ss", word);
+      case 1:
+        return String.format("1 %s", word);
+      default:
+        return String.format("%d %ss", count, word);
+    }
   }
 
 }
