450426c59a5e14238226082715a2e44d8e4192e6
Wil Boayue <wil.boayue@gmail.com>
2016-11-10 00:11:05 -0500

    go:clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,45 +1,38 @@
 package clock
 
 import "fmt"
+import "math"
 
 const testVersion = 4
 
 type Clock struct {
-  hour, minute int
+	hour, minute int
 }
 
 func floorDiv(x, y int) int {
-  return x / y
+	return int(math.Floor(float64(x) / float64(y)))
 }
 
 func floorMod(x, y int) int {
-  return x % y
+  return x - (floorDiv(x, y) * y)
 }
 
 func normalize(hour, minute int) (int, int) {
-  minute = floorMod(minute, 60)
-  hour = floorMod(hour + floorDiv(minute, 60), 24)
-  return hour, minute
+	minute = floorMod(minute, 60)
+	hour = floorMod(hour + floorDiv(minute, 60), 24)
+	return hour, minute
 }
 
 func New(hour, minute int) Clock {
-  hour, minute = normalize(hour, minute)
-  return Clock{hour, minute}
+	hour, minute = normalize(hour, minute)
+	return Clock{hour, minute}
 }
 
 func (c Clock) String() string {
-  return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
 }
 
 func (c Clock) Add(minutes int) Clock {
-  hour, minute := normalize(c.hour, c.minute + minutes)
-  return Clock{hour, minute}
+	hour, minute := normalize(c.hour, c.minute+minutes)
+	return Clock{hour, minute}
 }
-
-  // def __init__(self, hours, minutes):
-  //   self.minutes = minutes % 60
-  //   self.hours = (hours + (minutes // 60)) % 24
-
-
-  // def add(self, minutes):
-  //   return Clock(self.hours, self.minutes + minutes)
