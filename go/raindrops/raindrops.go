64581d289606e117d6ae9b539cd3dfe13b69fcb1
Wil Boayue <wil.boayue@gmail.com>
2016-11-15 01:22:23 -0500

    go:raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -5,24 +5,24 @@ import "strconv"
 const testVersion = 2
 
 type factor struct {
-    factor int
-    output string
+	factor int
+	output string
 }
 
-var factors = []factor {factor{3, "Pling"}, factor{5, "Plang"}, factor{7, "Plong"}}
+var factors = []factor{factor{3, "Pling"}, factor{5, "Plang"}, factor{7, "Plong"}}
 
 func Convert(x int) string {
-  converted := ""
+	converted := ""
 
-  for _, tuple := range(factors) {
-    if x % tuple.factor == 0 {
-      converted += tuple.output
-    }
-  }
+	for _, tuple := range factors {
+		if x%tuple.factor == 0 {
+			converted += tuple.output
+		}
+	}
 
-  if converted != "" {
-    return converted
-  } else {
-    return strconv.Itoa(x)
-  }
+	if converted != "" {
+		return converted
+	} else {
+		return strconv.Itoa(x)
+	}
 }
