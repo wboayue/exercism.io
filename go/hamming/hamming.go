6c15b02848fc1aa2086e2f4e2ee6d907f4938556
Wil Boayue <wil.boayue@gmail.com>
2016-11-15 00:22:12 -0500

    go:hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,23 +1,21 @@
 package hamming
 
-import (
-  "fmt"
-)
+import "fmt"
 
 const testVersion = 5
 
 func Distance(strand1, strand2 string) (int, error) {
-  if len(strand1) != len(strand2) {
-    return 0, fmt.Errorf("strands must be equal lengths")
-  }
+	if len(strand1) != len(strand2) {
+		return 0, fmt.Errorf("strands must be equal lengths")
+	}
 
-  hamming_distance := 0
+	hammingDistance := 0
 
-  for i, nucleotide := range(strand1) {
-    if nucleotide != rune(strand2[i]) {
-      hamming_distance += 1
-    }
-  }
+	for i, nucleotide := range strand1 {
+		if nucleotide != rune(strand2[i]) {
+			hammingDistance += 1
+		}
+	}
 
-  return hamming_distance, nil
+	return hammingDistance, nil
 }
