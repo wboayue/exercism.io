6aa635f50063a419ad2cd0d3ab418f6bca0b4f30
Wil Boayue <wil.boayue@gmail.com>
2016-11-15 01:20:49 -0500

    go:raindrops

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,28 @@
+package raindrops
+
+import "strconv"
+
+const testVersion = 2
+
+type factor struct {
+    factor int
+    output string
+}
+
+var factors = []factor {factor{3, "Pling"}, factor{5, "Plang"}, factor{7, "Plong"}}
+
+func Convert(x int) string {
+  converted := ""
+
+  for _, tuple := range(factors) {
+    if x % tuple.factor == 0 {
+      converted += tuple.output
+    }
+  }
+
+  if converted != "" {
+    return converted
+  } else {
+    return strconv.Itoa(x)
+  }
+}
