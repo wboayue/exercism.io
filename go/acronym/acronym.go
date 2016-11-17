3d85e6e746ad184fc64814acc59e4a45de621243
Wil Boayue <wil.boayue@gmail.com>
2016-11-16 23:31:28 -0500

    go:acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -6,23 +6,23 @@ import "unicode"
 const testVersion = 1
 
 func isSeparator(char rune) bool {
-  return char == ' ' || char == '-'
+	return char == ' ' || char == '-'
 }
 
 func isMixedCased(previous, current rune) bool {
-  return unicode.IsLower(previous) && unicode.IsUpper(current)
+	return unicode.IsLower(previous) && unicode.IsUpper(current)
 }
 
 func abbreviate(phrase string) string {
-  previous := ' '
-  acronym := []string{}
+	previous := ' '
+	acronym := []string{}
 
-  for _, current := range phrase {
-    if isSeparator(previous) || isMixedCased(previous, current) {
-      acronym = append(acronym, string(current))
-    }
-    previous = current
-  }
+	for _, current := range phrase {
+		if isSeparator(previous) || isMixedCased(previous, current) {
+			acronym = append(acronym, string(current))
+		}
+		previous = current
+	}
 
-  return strings.ToUpper(strings.Join(acronym, ""))
+	return strings.ToUpper(strings.Join(acronym, ""))
 }
