88777da18f81f10f1de107d41f6565eeecf89873
Wil Boayue <wboayue@targetedvictory.com>
2016-11-21 15:26:46 -0500

    lua:difference-of-squares

---
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.square_of_sums = function(x)
+  sum = 0
+  for i = 1, x do
+    sum = sum + i
+  end
+  return sum * sum
+end
+
+M.sum_of_squares = function(x)
+  sum = 0
+  for i = 1, x do
+    sum = sum + i * i
+  end
+  return sum
+end
+
+M.difference_of_squares = function(x)
+  return M.square_of_sums(x) - M.sum_of_squares(x)
+end
+
+return M
