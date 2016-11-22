3d3e11c8fae4869400aade19d0f4594e41fb03c9
Wil Boayue <wil.boayue@gmail.com>
2016-11-22 10:32:12 -0500

    lua:anagram

--- lua/anagram/anagram.lua
+++ lua/anagram/anagram.lua
@@ -1,26 +1,23 @@
 local Anagram = {}
 Anagram.__index = Anagram
 
-local function word_bag(word)
-  local bag = {}
+local function sort_letters(word)
+  local letters = {}
   for c in string.lower(word):gmatch(".") do
-    table.insert(bag, c)
+    table.insert(letters, c)
   end
-  table.sort(bag)
-  return table.concat(bag)
+  table.sort(letters)
+  return table.concat(letters)
 end
 
 function Anagram:new(word)
-  local detector = {}
-  setmetatable(detector, Anagram)
-  detector.word_bag = word_bag(word)
-  return detector
+  return setmetatable({sorted_letters = sort_letters(word)}, Anagram)
 end
 
 function Anagram:match(words)
   local anagrams = {}
   for _, candidate in ipairs(words) do
-    if self.word_bag == word_bag(candidate) then
+    if self.sorted_letters == sort_letters(candidate) then
       table.insert(anagrams, candidate)
     end
   end
