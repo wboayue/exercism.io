024944406837f50af14efbafeab1f1cfb4f522c4
Wil Boayue <wil.boayue@gmail.com>
2016-11-22 16:10:02 -0500

    lua:octal

--- lua/octal/octal.lua
+++ lua/octal/octal.lua
@@ -1,26 +1,16 @@
-local function to_digits(number)
-  local digits = {}
-  for digit in string.gmatch(number, ".") do
-    table.insert(digits, tonumber(digit))
-  end
-  return digits
-end
-
 local function is_valid(number)
   return string.match(number, '^[0-7]+$')
 end
 
 local function to_decimal(number)
-  if is_valid(number) then
-    local digits = to_digits(number)
-    local decimal = 0
-    for _, digit in ipairs(digits) do
-      decimal = (decimal * 8) + digit
-    end
-    return decimal
-  else
-    return 0
+  if not is_valid(number) then return 0 end
+
+  local decimal = 0
+  for digit in string.gmatch(number, ".") do
+    decimal = (decimal * 8) + tonumber(digit)
   end
+  
+  return decimal
 end
 
 return function(number)
