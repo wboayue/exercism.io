43401563cab38bfc6239c48b4cab2c1fdf54a09c
Wil Boayue <wil.boayue@gmail.com>
2016-11-27 01:08:52 -0500

    lua:word-count

--- lua/word-count/word-count.lua
+++ lua/word-count/word-count.lua
@@ -1,6 +1,6 @@
 function word_count(sentence)
   local counts = {}
-  for word in string.gmatch(string.lower(sentence), "[a-z0-9]+") do
+  for word in string.gmatch(string.lower(sentence), "%w+") do
     counts[word] = (counts[word] or 0) + 1
   end
   return counts
