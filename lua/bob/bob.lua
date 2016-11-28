e459be7680ba35a53200ffa093ef0585c1498fa3
Wil Boayue <wil.boayue@gmail.com>
2016-11-28 00:10:40 -0500

    lua:bob

--- lua/bob/bob.lua
+++ lua/bob/bob.lua
@@ -1,23 +1,23 @@
-local M = {}
+local bob = {}
 
 local function is_silence(sentence)
-  return string.gsub(sentence, " ", "") == ""
+  return sentence:gsub(' ', '') == ''
 end
 
 local function is_shouting(sentence)
-  return string.upper(sentence) == sentence
+  return sentence:upper() == sentence
 end
 
 local function is_question(sentence)
-  return string.sub(sentence, -1) == "?"
+  return sentence:sub(-1) == '?'
 end
 
-M.hey = function(sentence)
-  if is_silence(sentence) then return "Fine, be that way."
-  elseif is_shouting(sentence) then return "Whoa, chill out!"
-  elseif is_question(sentence) then return "Sure"
-  else return "Whatever" 
+bob.hey = function(sentence)
+  if is_silence(sentence) then return 'Fine, be that way.'
+  elseif is_shouting(sentence) then return 'Whoa, chill out!'
+  elseif is_question(sentence) then return 'Sure'
+  else return 'Whatever'
   end
 end
 
-return M
+return bob
