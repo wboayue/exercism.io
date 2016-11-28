852c1c0cc5481e69e1a80379527483d5d8485426
Wil Boayue <wil.boayue@gmail.com>
2016-11-27 20:13:44 -0500

    lua:pangram

--- lua/pangram/pangram.lua
+++ lua/pangram/pangram.lua
@@ -1,6 +1,3 @@
-local alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-
 local function count_letters(sentence)
   local counts = {}
   for letter in string.gmatch(string.lower(sentence), "[a-z]") do
@@ -9,14 +6,15 @@ local function count_letters(sentence)
   return counts
 end
 
-local function contains_all(counts, alphabet)
-  for _, letter in pairs(alphabet) do
+local function contains_all_aplhabet(counts)
+  local alphabet = string.gmatch('abcdefghijklmnopqrstuvwxyz', '.')
+
+  for letter in alphabet do
     if not counts[letter] then return false end
   end
   return true
 end
 
 return function(sentence)
-  local counts = count_letters(sentence)
-  return contains_all(counts, alphabet)
+  return contains_all_aplhabet(count_letters(sentence))
 end
