97c8783d26824003b6e4dfe03cff8659a0006cbe
Wil Boayue <wil.boayue@gmail.com>
2016-11-27 23:36:56 -0500

    lua:pangram

--- lua/pangram/pangram.lua
+++ lua/pangram/pangram.lua
@@ -1,13 +1,14 @@
 local function count_letters(sentence)
   local counts = {}
-  for letter in string.gmatch(string.lower(sentence), "[a-z]") do
+  for letter in sentence:lower():gmatch('[a-z]') do
     counts[letter] = (counts[letter] or 0) + 1
   end
   return counts
 end
 
 local function contains_all_alphabet(counts)
-  for letter in string.gmatch('abcdefghijklmnopqrstuvwxyz', '.') do
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  for letter in alphabet:gmatch('.') do
     if not counts[letter] then return false end
   end
   return true
