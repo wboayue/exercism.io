de6c25ee288af52c0107855e3edbda48449f22a0
Wil Boayue <wboayue@crossscreen.media>
2017-05-18 20:58:02 -0400

    haskell/leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,9 +2,9 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | isDivisibleBy year 400 = True
-  | isDivisibleBy year 100 = False
-  | isDivisibleBy year 4 = True
+  | isYearDivisibleBy 400 = True
+  | isYearDivisibleBy 100 = False
+  | isYearDivisibleBy 4 = True
   | otherwise = False
   where
-    isDivisibleBy x y = rem x y == 0
+    isYearDivisibleBy x = rem year x == 0
