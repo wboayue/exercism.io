b1eb83a20c1d2c88d5fa718af4918c9662ebcce3
Wil Boayue <wil.boayue@gmail.com>
2017-05-20 22:08:53 -0400

    haskell/bob

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,15 @@
+module Bob (responseFor) where
+
+import Data.Char
+
+responseFor :: String -> String
+responseFor xs
+  | yell      = "Whoa, chill out!"
+  | question  = "Sure."
+  | silence   = "Fine. Be that way!"
+  | otherwise = "Whatever."
+  where
+    yell     = (any isAlpha xs) && (map toUpper xs) == xs
+    question = not (null stripped) && last stripped == '?' 
+    silence  = all isSpace xs
+    stripped = reverse (dropWhile isSpace (reverse xs))
