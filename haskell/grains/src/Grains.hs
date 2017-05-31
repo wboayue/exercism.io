f9f8fa21fc4bb0b8d047dc423069678afc51bbb0
Wil Boayue <wboayue@crossscreen.media>
2017-05-31 17:11:58 -0400

    haskell/grains

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -4,8 +4,8 @@ import Data.Bits
 
 square :: Integer -> Maybe Integer
 square n
-  | n < 1 || n > 64 = Nothing
-  | otherwise       = Just (grains n)
+  | n `elem` [1..64] = Just (grains n)
+  | otherwise        = Nothing
 
 grains :: Integer -> Integer
 grains n = 2 ^ (n - 1)
