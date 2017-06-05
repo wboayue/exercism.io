9d893cfabf6089947733758be50298f345c7fa1c
Wil Boayue <wboayue@crossscreen.media>
2017-06-05 15:43:14 -0400

    haskell/nucleotide-count

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -6,9 +6,9 @@ initialCounts :: Map Char Int
 initialCounts = fromList [ ('A', 0), ('C', 0), ('G', 0), ('T', 0) ]
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts xs = if isValidNucleotide
-                      then Right $ foldl addToCounts initialCounts xs
-                      else Left "invalid nucleotide."
+nucleotideCounts xs
+  | isValidNucleotide = Right $ foldl addToCounts initialCounts xs
+  | otherwise         = Left "invalid nucleotide."
   where
     addToCounts counts x = insertWith (+) x 1 counts
     isValidNucleotide    = all (`elem` keys initialCounts) xs
