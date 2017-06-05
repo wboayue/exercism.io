dfe885dcd24206580799d8f3aac1d766b77ac97f
Wil Boayue <wboayue@crossscreen.media>
2017-06-05 11:20:31 -0400

    haskell/hamming

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -5,8 +5,8 @@ distance xs ys = hamming xs ys 0
 
 hamming :: String -> String -> Int -> Maybe Int
 hamming [] [] d = Just d
-hamming (x:xs) [] _ = Nothing
-hamming [] (y:ys) _ = Nothing
+hamming _  [] _ = Nothing
+hamming [] _  _ = Nothing
 hamming (x:xs) (y:ys) d
   | x == y    = hamming xs ys d
   | otherwise = hamming xs ys (d + 1)
