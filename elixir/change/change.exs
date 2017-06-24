acde74b766e01ab50acbb9daa60d7a2419f1bc19
Wil Boayue <wil.boayue@gmail.com>
2017-06-24 00:25:52 -0400

    elixir/change

---
+++ elixir/change/change.exs
@@ -0,0 +1,42 @@
+defmodule Change do
+  @doc """
+    Determine the least number of coins to be given to the user such
+    that the sum of the coins' value would equal the correct amount of change.
+    It returns {:error, "cannot change"} if it is not possible to compute the
+    right amount of coins. Otherwise returns the tuple {:ok, list_of_coins}
+
+    ## Examples
+
+      iex> Change.generate([5, 10, 15], 3)
+      {:error, "cannot change"}
+
+      iex> Change.generate([1, 5, 10], 18)
+      {:ok, [1, 1, 1, 5, 10]}
+
+  """
+
+  @spec generate(list, integer) :: {:ok, list} | {:error, String.t}
+  def generate(coins, target) do
+    permute(coins, target, [])
+  end
+
+  def duplicate(coin, 0), do: []
+  def duplicate(coin, n) do
+    (1..n) |> Enum.map(fn _ -> coin end)  
+  end
+
+  def permute([coin], target, candidate) do
+    if rem(target, coin) == 0 do
+      candidate ++ duplicate(coin, div(target, coin))
+    else
+      []
+    end
+  end
+
+  def permute([coin | coins], target, candidate) do
+    (0..div(target, coin))
+    |> Enum.map(fn i ->
+      permute(coins, target - coin * i, candidate ++ duplicate(coin, i))
+    end)
+  end
+end
