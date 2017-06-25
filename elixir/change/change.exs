4cad38d2faacd5766186e9f42df23e7854c1d2fe
Wil Boayue <wil.boayue@gmail.com>
2017-06-25 00:40:52 -0400

    elixir/change

--- elixir/change/change.exs
+++ elixir/change/change.exs
@@ -16,18 +16,25 @@ defmodule Change do
   """
 
   @spec generate(list, integer) :: {:ok, list} | {:error, String.t}
+  def generate(coins, 0), do: {:ok, []} 
+  def generate(coins, target) when target < 1, do: {:error, "cannot change"} 
   def generate(coins, target) do
-    permute(coins, target, [])
+    candidates = permute(coins, target, [])
+    if candidates == [] do
+      {:error, "cannot change"}
+    else
+      {:ok, Enum.min_by(candidates, &Enum.count(&1))}
+    end
   end
 
-  def duplicate(coin, 0), do: []
+  def duplicate(_coin, 0), do: []
   def duplicate(coin, n) do
     (1..n) |> Enum.map(fn _ -> coin end)  
   end
 
   def permute([coin], target, candidate) do
     if rem(target, coin) == 0 do
-      candidate ++ duplicate(coin, div(target, coin))
+      [candidate ++ duplicate(coin, div(target, coin))]
     else
       []
     end
@@ -35,8 +42,8 @@ defmodule Change do
 
   def permute([coin | coins], target, candidate) do
     (0..div(target, coin))
-    |> Enum.map(fn i ->
-      permute(coins, target - coin * i, candidate ++ duplicate(coin, i))
+    |> Enum.reduce([], fn i, acc ->
+      acc ++ permute(coins, target - coin * i, candidate ++ duplicate(coin, i))
     end)
   end
 end
