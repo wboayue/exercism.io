24abc918cc068ab3489b5fe164346b8f174095da
Wil Boayue <wil.boayue@gmail.com>
2017-07-16 22:17:41 -0400

    python/isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,13 +1,13 @@
-import string
-
 def is_isogram(phrase):
   seen = set()
 
   for x in phrase.lower():
-    if x.isalpha():
-      if x in seen:
-        return False
-      else:
-        seen.add(x)
+    if not x.isalpha():
+      continue
+
+    if x in seen:
+      return False
+    
+    seen.add(x)
 
   return True
