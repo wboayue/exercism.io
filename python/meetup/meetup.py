75b246f1ada9017cc75b562d96fcb1a13cd68392
Wil Boayue <wil.boayue@gmail.com>
2017-07-18 23:57:41 -0400

    python/meetup

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,31 +1,33 @@
 from datetime import date
 from calendar import monthrange
 
-week_days = { 'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6 }
+weekday_to_ord = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
 
-def first_day(year, month, week_day):
-  first_day_of_month = date(year=year, month=month, day=1)
-  days_between = (week_days[week_day] - first_day_of_month.weekday()) % 7
-  return first_day_of_month.replace(day=1 + days_between)
+def first_day(year, month, weekday):
+  first_weekday, _last_day = monthrange(year, month)
+  return 1 + ((weekday_to_ord[weekday] - first_weekday) % 7)
 
-def weekdays(year, month, week_day):
-  first = first_day(year, month, week_day)
-  day = first.day
-  wd, last_day = monthrange(year, month)
+def matching_weekdays(year, month, weekday):
+  current_day = first_day(year, month, weekday)
+  _first_weekday, last_day = monthrange(year, month)
 
-  while day <= last_day:
-    yield first.replace(day=day)
-    day += 7
+  results = []
 
-scheduler = {
-  '1st': lambda x: x[0],
-  '2nd': lambda x: x[1],
-  '3rd': lambda x: x[2],
-  '4th': lambda x: x[3],
-  '5th': lambda x: x[4],
-  'last': lambda x: x[-1],
-  'teenth': lambda dates: [date for date in dates if date.day in range(13, 20)][0],
-}
+  while current_day <= last_day:
+    results.append(date(year, month, current_day))
+    current_day += 7
 
-def meetup_day(year, month, week_day, schedule):
-  return scheduler[schedule](list(weekdays(year, month, week_day)))
+  return results
+
+def meetup_day(year, month, weekday, schedule):
+  select_day = {
+      '1st': lambda dates: dates[0],
+      '2nd': lambda dates: dates[1],
+      '3rd': lambda dates: dates[2],
+      '4th': lambda dates: dates[3],
+      '5th': lambda dates: dates[4],
+      'last': lambda dates: dates[-1],
+      'teenth': lambda dates: next(date for date in dates if date.day in range(13, 20))
+  }
+
+  return select_day[schedule](matching_weekdays(year, month, weekday))
