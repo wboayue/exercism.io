601e30b50539a3b9d075b0197f0bfb50c55f43aa
Wil Boayue <wil.boayue@gmail.com>
2017-07-19 20:54:06 -0400

    python/meetup

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,33 +1,31 @@
-from datetime import date
-from calendar import monthrange
+from calendar import Calendar
+from functools import partial
 
 weekday_to_ord = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
 
-def first_day(year, month, weekday):
-  first_weekday, _last_day = monthrange(year, month)
-  return 1 + ((weekday_to_ord[weekday] - first_weekday) % 7)
-
 def matching_weekdays(year, month, weekday):
-  current_day = first_day(year, month, weekday)
-  _first_weekday, last_day = monthrange(year, month)
-
-  results = []
-
-  while current_day <= last_day:
-    results.append(date(year, month, current_day))
-    current_day += 7
-
-  return results
+  return [date
+          for date in Calendar().itermonthdates(year, month)
+          if date.month == month and date.weekday() == weekday_to_ord[weekday]]
 
 def meetup_day(year, month, weekday, schedule):
+  def nth(n, dates):
+    return dates[n]
+
+  def last(dates):
+    return dates[-1]
+
+  def teenth(dates):
+    return next(date for date in dates if date.day in range(13, 20))
+
   select_day = {
-      '1st': lambda dates: dates[0],
-      '2nd': lambda dates: dates[1],
-      '3rd': lambda dates: dates[2],
-      '4th': lambda dates: dates[3],
-      '5th': lambda dates: dates[4],
-      'last': lambda dates: dates[-1],
-      'teenth': lambda dates: next(date for date in dates if date.day in range(13, 20))
+      '1st': partial(nth, 0),
+      '2nd': partial(nth, 1),
+      '3rd': partial(nth, 2),
+      '4th': partial(nth, 3),
+      '5th': partial(nth, 4),
+      'last': last,
+      'teenth': teenth
   }
 
   return select_day[schedule](matching_weekdays(year, month, weekday))
