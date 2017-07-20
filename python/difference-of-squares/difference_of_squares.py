e0abcf037d7b10fb9f1b3d556264265572c644da
Wil Boayue <wil.boayue@gmail.com>
2017-07-19 22:53:32 -0400

    python/difference-of-squares

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,8 +1,8 @@
 def square_of_sum(x):
-  return sum(range(1, x + 1)) ** 2
+  return sum(range(x + 1)) ** 2
 
 def sum_of_squares(x):
-  return sum(y ** 2 for y in range(1, x + 1))
+  return sum(y ** 2 for y in range(x + 1))
 
 def difference(x):
   return square_of_sum(x) - sum_of_squares(x)
