cb0ff4a14611024406d4a6c583c0736b78ace9ae
Wil Boayue <wil.boayue@gmail.com>
2017-07-19 23:40:46 -0400

    python/anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,8 +1,5 @@
 def detect_anagrams(word, candidates):
   def is_anagram(candidate):
-    if word.lower() == candidate.lower():
-      return False
-    else:
-      return sorted(word.lower()) == sorted(candidate.lower())
+    return sorted(word.lower()) == sorted(candidate.lower()) and word.lower() != candidate.lower()
 
   return [candidate for candidate in candidates if is_anagram(candidate)]
