2a25c2b8da74c1462074ac1ab90f712b1f3ce649
Wil Boayue <wil.boayue@gmail.com>
2017-07-22 16:04:10 -0400

    python/allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,20 +1,20 @@
-ITEMS = {
-    'eggs':  1,
-    'peanuts': 2,
-    'shellfish': 4,
-    'strawberries': 8,
-    'tomatoes': 16,
-    'chocolate': 32,
-    'pollen': 64,
-    'cats': 128
-}
-
 class Allergies(object):
+  ITEMS = {
+      'eggs':  1,
+      'peanuts': 2,
+      'shellfish': 4,
+      'strawberries': 8,
+      'tomatoes': 16,
+      'chocolate': 32,
+      'pollen': 64,
+      'cats': 128
+  }
+
   def __init__(self, allergens):
     self._allergens = allergens
     self.lst = [item
-                for item in ITEMS.keys()
+                for item in self.ITEMS.keys()
                 if self.is_allergic_to(item)]
 
   def is_allergic_to(self, item):
-    return self._allergens & ITEMS[item] > 0
+    return bool(self._allergens & self.ITEMS[item])
