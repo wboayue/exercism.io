92f7318d4d1518a86f948f58b5d22ebd944d2d6a
Wil Boayue <wboayue@crossscreen.media>
2017-07-26 10:41:00 -0400

    python/sieve

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,14 +1,20 @@
+from enum import Enum
+
 def sieve(up_to):
   if up_to < 2:
     return []
 
   return [x for x in _generate_primes(up_to)]
 
+class NumberType(Enum):
+  PRIME = 1
+  COMPOSITE = 2
+
 def _generate_primes(up_to):
-  candidates = [True] * (up_to+1)
+  candidates = [NumberType.PRIME] * (up_to+1)
 
   for i in range(2, up_to+1):
-    if not candidates[i]:
+    if candidates[i] == NumberType.COMPOSITE:
       continue
 
     _mark_non_primes(candidates, i, up_to)
@@ -17,4 +23,4 @@ def _generate_primes(up_to):
 
 def _mark_non_primes(candidates, prime, up_to):
   for i in range(prime, up_to+1, prime):
-    candidates[i] = False
+    candidates[i] = NumberType.COMPOSITE
