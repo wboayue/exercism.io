87d3522afc160882104ba30b5ca61543a2881b57
Wil Boayue <wboayue@crossscreen.media>
2017-07-27 15:32:08 -0400

    python/atbash-cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,34 +1,23 @@
 from collections import defaultdict
-# from bytes import maketrans
-
-def _build_encoder_and_decoder():
-  mapping = zip(
-      'abcdefghijklmnopqrstuvwxyz0123456789',
-      'zyxwvutsrqponmlkjihgfedcba0123456789')
-
-  encode = defaultdict(None)
-  decode = defaultdict()
-
-  for x, y in mapping:
-    encode[x] = y
-    decode[y] = x
-
-  return (encode, decode)
-
-_ENCODER, _DECODER = _build_encoder_and_decoder()
-
-def _make_translator(table):
-  def translator(x):
-    if x in table:
-      return table[x]
-    else:
-      return ''
-  return translator
 
 def encode(plain):
-  encode = _make_translator(_ENCODER)
-  return ''.join([encode(x) for x in plain.lower()])
+  encoder = _make_translator('abcdefghijklmnopqrstuvwxyz')
+  return _format_cipher(''.join(encoder[x] for x in plain.lower()))
 
 def decode(cipher):
-  decode = _make_translator(_DECODER)
-  return ''.join([decode(x) for x in cipher])
+  decoder = _make_translator('zyxwvutsrqponmlkjihgfedcba')
+  return ''.join(decoder[x] for x in cipher)
+
+def _make_translator(key):
+  table = defaultdict(lambda: '')
+
+  for x, y in zip(key, key[::-1]):
+    table[x] = y
+
+  for x  in '0123456789':
+    table[x] = x
+
+  return table
+
+def _format_cipher(cipher):
+  return ' '.join(cipher[i:i+5] for i in range(0, len(cipher), 5))
