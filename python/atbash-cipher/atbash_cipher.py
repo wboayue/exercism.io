b9a88e34322319936f102af50c2a76f91c53ad1e
Wil Boayue <wboayue@crossscreen.media>
2017-07-27 15:36:11 -0400

    python/atbash-cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+GROUP_SIZE = 5
+
 def encode(plain):
   encoder = _make_translator('abcdefghijklmnopqrstuvwxyz')
   return _format_cipher(''.join(encoder[x] for x in plain.lower()))
@@ -20,4 +22,4 @@ def _make_translator(key):
   return table
 
 def _format_cipher(cipher):
-  return ' '.join(cipher[i:i+5] for i in range(0, len(cipher), 5))
+  return ' '.join(cipher[i:i+GROUP_SIZE] for i in range(0, len(cipher), GROUP_SIZE))
