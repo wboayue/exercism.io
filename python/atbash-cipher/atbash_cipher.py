daf2326dfa6d27c4de53b0b693965f421bda043b
Wil Boayue <wboayue@crossscreen.media>
2017-07-27 16:55:27 -0400

    python/atbash-cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,16 +1,17 @@
 from collections import defaultdict
 from textwrap import wrap
+import string
 
 GROUP_SIZE = 5
 
 def encode(plain):
-  return _format_cipher(_rotate(plain.lower()), GROUP_SIZE)
+  return _format_cipher(_translate(plain.lower()), GROUP_SIZE)
 
 def decode(cipher):
-  return _rotate(cipher)
+  return _translate(cipher)
 
-def _rotate(text):
-  mapper = _make_translator('abcdefghijklmnopqrstuvwxyz')
+def _translate(text):
+  mapper = _make_translator(string.ascii_lowercase)
   return ''.join(mapper[x] for x in text)
 
 def _make_translator(key):
@@ -19,7 +20,7 @@ def _make_translator(key):
   for x, y in zip(key, key[::-1]):
     table[x] = y
 
-  for x  in '0123456789':
+  for x  in string.digits:
     table[x] = x
 
   return table
