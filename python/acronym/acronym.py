dda690cf5bfc62c6c3af7ff2a1e971798979d154
Wil Boayue <wboayue@crossscreen.media>
2017-07-28 11:28:45 -0400

    python/acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,4 +1,4 @@
-import re
+from re import split
 
 def abbreviate(phrase):
-  return ''.join(word[0].upper() for word in re.split(r'[ -]', phrase))
+  return ''.join(word[0] for word in split(r'[ -]', phrase)).upper()
