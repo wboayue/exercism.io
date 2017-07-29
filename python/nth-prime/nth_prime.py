88a8bfb37953978399e48fc5567074b86e06c80f
Wil Boayue <wil.boayue@gmail.com>
2017-07-29 12:57:02 -0400

    python/nth-prime

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,31 +1,30 @@
 from math import log
-from enum import Enum
 
-def nth_prime(n):
+def nth_prime(n: int) -> int:
   if n == 0:
     raise ValueError
 
-  limit = _approximate_limit(n)
+  return _generate_primes(_limit(n))[n-1]
 
-  return list(_generate_primes(limit))[n-1]
-
-def _approximate_limit(n):
+def _limit(n: int) -> int:
+  """Calculate upper bound for value of nth prime"""
   if n < 6:
     n = 6
 
   return int(n*log(n) + n*log(log(n)))
 
-def _generate_primes(up_to):
-  candidates = [False] * (up_to+1)
+def _generate_primes(up_to: int) -> list:
+  primes, candidates = list(), [False] * (up_to+1)
 
   for i in range(2, up_to+1):
     if candidates[i]:
       continue
 
+    primes.append(i)
     _mark_composites(candidates, i, up_to)
 
-    yield i
+  return primes
 
-def _mark_composites(candidates, prime, up_to):
+def _mark_composites(candidates: list, prime: int, up_to: int) -> None:
   for i in range(prime*2, up_to+1, prime):
     candidates[i] = True
