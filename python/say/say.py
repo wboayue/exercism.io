cc7fdc1161ca3fb8591aaf29b40da3fc77e2ae88
Wil Boayue <wil.boayue@gmail.com>
2017-07-30 23:50:07 -0400

    python/say

--- python/say/say.py
+++ python/say/say.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 _base = ['zero', 'one', 'two', 'three', 'four', 'five',
          'six', 'seven', 'eight', 'nine', 'ten',
          'eleven', 'twelve', 'thirtheen', 'fourteen', 'fifteen',
@@ -5,52 +7,54 @@ _base = ['zero', 'one', 'two', 'three', 'four', 'five',
 
 _tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
+_thousands = ['', 'thousand', 'million', 'billion']
+
 def say(n):
   if n < 0:
     raise AttributeError('number must be positive')
 
   if n >= 1_000_000_000_000:
-    raise AttributeError('number must be less than 1 billion')
+    raise AttributeError('number must be less than 1 trillion')
 
-  if n >= 1_000_000_000:
-    x, y = divmod(n, 1_000_000_000)
-    if y == 0:
-      return '{} billion'.format(say(int(x)))
+  if n == 0:
+    return 'zero'
+
+  def format(i, x):
+    if i < 1:
+      return '{}'.format(_to_words(x))
     else:
-      if y <= 10:
-        return '{} billion and {}'.format(say(x), say(y))
-      else:
-        return '{} billion {}'.format(say(x), say(y))
+      return '{} {}'.format(_to_words(x), _thousands[i])
 
+  words = [format (i, x)
+          for i, x in enumerate(_split_into_thousands(n))
+          if x]
+  if len(words) > 1 and not 'and' in words[0]:
+    words.insert(1, 'and')
+
+  words.reverse()
+  return ' '.join(words)
+
+def _to_words(n):
   if n <= 20:
-    return _base[n]
+    return _base[int(n)]
 
   if n < 100:
     x, y = divmod(n, 10)
-    return '{}-{}'.format(_tens[x], say(y))
+    return '{}-{}'.format(_tens[int(x)], _to_words(y))
 
-  if n < 1000:
-    x, y = divmod(n, 100)
-    if y == 0:
-      return '{} hundred'.format(_base[x])
-    else:
-      return '{} hundred and {}'.format(_base[x], say(y))
+  x, y = divmod(n, 100)
+  if y == 0:
+    return '{} hundred'.format(_base[int(x)])
+  else:
+    return '{} hundred and {}'.format(_base[int(x)], _to_words(y))
 
-  if n < 1_000_000:
-    x, y = divmod(n, 1000)
-    if y == 0:
-      return '{} thousand'.format(say(x))
-    else:
-      return '{} thousand {}'.format(say(x), say(y))
+def _split_into_thousands(n):
+  x, y = divmod(n, 1000)
 
-  if n < 1_000_000_000:
-    x, y = divmod(n, 1_000_000)
-    if y == 0:
-      return '{} million'.format(say(int(x)))
-    else:
-      if y <= 10:
-        return '{} million and {}'.format(say(x), say(y))
-      else:
-        return '{} million {}'.format(say(x), say(y))
-
-
+  thousands = list([y])
+  
+  while x:
+    x, y = divmod(x, 1000)
+    thousands.append(y)
+  
+  return thousands
