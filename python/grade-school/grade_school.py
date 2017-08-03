e132e2c7e79ae7561543a037587e6e98cd7de7bd
Wil Boayue <wil.boayue@gmail.com>
2017-08-02 23:51:24 -0400

    python/grade-school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -3,13 +3,13 @@ from collections import defaultdict, OrderedDict
 class School(object):
   def __init__(self, name):
     self._name = name
-    self._roster = defaultdict(set)
+    self._roster = defaultdict(list)
 
   def add(self, student, grade):
-    self._roster[grade].add(student)
+    self._roster[grade].append(student)
 
   def grade(self, num):
-    return set(self._roster[num])
+    return list(self._roster[num])
 
   def sort(self):
-    return OrderedDict((grade, tuple(sorted(students))) for grade, students in self._roster.items())
+    return OrderedDict((grade, tuple(sorted(students))) for grade, students in sorted(self._roster.items()))
