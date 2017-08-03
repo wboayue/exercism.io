9a197ca267f5ec208367ee6f8984239a6ddf9ed3
Wil Boayue <wboayue@crossscreen.media>
2017-08-03 12:03:43 -0400

    python/hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def distance(strand1, strand2):
-  return sum([x != y for (x, y) in zip(strand1, strand2)])
+  if len(strand1) != len(strand2):
+    raise ValueError('strands must be equals lengths')
+
+  return sum(x != y for x, y in zip(strand1, strand2))
