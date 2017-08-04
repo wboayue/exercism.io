ab14b3b9b88ae1a172abbe26293e158fc12e1301
Wil Boayue <wboayue@crossscreen.media>
2017-08-03 23:25:47 -0400

    python/space-age

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from functools import partial
 
 BASE = 31557600 # base earth orbital period
 
@@ -22,29 +23,9 @@ class SpaceAge(object):
   def __init__(self, seconds):
     self.seconds = seconds
 
+    for planet in Planet:
+      method_name = "on_{}".format(planet.name.lower())
+      setattr(self, method_name, partial(self.on, planet))
+
   def on(self, planet):
     return planet.space_age(self.seconds)
-
-  def on_earth(self):
-    return self.on(Planet.EARTH)
-
-  def on_mercury(self):
-    return self.on(Planet.MERCURY)
-
-  def on_venus(self):
-    return self.on(Planet.VENUS)
-
-  def on_mars(self):
-    return self.on(Planet.MARS)
-
-  def on_jupiter(self):
-    return self.on(Planet.JUPITER)
-
-  def on_saturn(self):
-    return self.on(Planet.SATURN)
-
-  def on_uranus(self):
-    return self.on(Planet.URANUS)
-
-  def on_neptune(self):
-    return self.on(Planet.NEPTUNE)
