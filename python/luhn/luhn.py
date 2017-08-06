32060ccf414cb97b2152dd0315d36c7f6fe0e23a
Wil Boayue <wil.boayue@gmail.com>
2017-08-06 15:24:41 -0400

    python/luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -5,18 +5,15 @@ class Luhn(object):
     self._numbers = [ch for ch in number if not ch.isspace()]
 
   def is_valid(self):
-    if len(self._numbers) < 2 or not all_digits(self._numbers):
+    if len(self._numbers) < 2 or not all(c in string.digits for c in self._numbers):
       return False
 
     digits = [int(num) for num in self._numbers]
-    digits = double_second_digits(digits)
+    digits = _double_second_digits(digits)
 
     return sum(digits) % 10 == 0
 
-def all_digits(numbers):
-  return all(c in string.digits for c in numbers)
-
-def double_second_digits(digits):
+def _double_second_digits(digits):
   digits = list(digits)
 
   for i in range(len(digits) -2, -1, -2):
