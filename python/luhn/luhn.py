568ce34f15b2db16d6c34154b5e6cd8d92b4a733
Wil Boayue <wil.boayue@gmail.com>
2017-08-06 21:41:53 -0400

    python/luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,15 +1,12 @@
-import string
-
 class Luhn(object):
   def __init__(self, number):
     self._numbers = [ch for ch in number if not ch.isspace()]
-    self._all_digits = all(c in string.digits for c in self._numbers)
 
   def is_valid(self):
-    if not (len(self._numbers) > 1 and self._all_digits):
+    if not (len(self._numbers) > 1 and all(c.isdigit() for c in self._numbers)):
       return False
 
-    digits = [int(digit) for digit in self._numbers]
+    digits = map(int, self._numbers)
   
     for i in range(len(digits)-2, -1, -2):
       digits[i] = digits[i] * 2
