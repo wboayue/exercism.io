9a8240be9e760db17b3014b7898590474f597427
Wil Boayue <wilfred.boayue@airbus-sv.com>
2019-06-19 13:00:19 -0700

    go fmt

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,19 +1,19 @@
 package acronym
 
 import (
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 )
 
 const testVersion = 1
 
 func Abbreviate(phrase string) string {
-    re := regexp.MustCompile(`(\A|[ _-])+(\p{L})`)
+	re := regexp.MustCompile(`(\A|[ _-])+(\p{L})`)
 	acronym := []string{}
 
-    for _, match := range re.FindAllStringSubmatch(phrase, -1) {
-    	acronym = append(acronym, match[2])
-    }
+	for _, match := range re.FindAllStringSubmatch(phrase, -1) {
+		acronym = append(acronym, match[2])
+	}
 
 	return strings.ToUpper(strings.Join(acronym, ""))
 }
