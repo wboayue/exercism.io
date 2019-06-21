28ea7a7814006a0bce553f96923d04c975955830
Wil Boayue <wilfred.boayue@airbus-sv.com>
2019-06-21 13:12:20 -0700

    go/space-age use map

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,42 +1,27 @@
 package space
 
-import "fmt"
+type Planet string
 
 const secondsInYear = 31557600
 
-type Planet string
+var orbitalPeriod = map[Planet]float64{
+	"Earth":   1,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
 
 // Given an age in seconds,
 // calculate how old someone is on specified planet
 func Age(seconds float64, planet Planet) float64 {
-	return earthAge(seconds) / orbitalPeriod(planet)
+	return earthAge(seconds) / orbitalPeriod[planet]
 }
 
 // converts seconds to earth years
 func earthAge(seconds float64) float64 {
 	return seconds / secondsInYear
-}
-
-// determines the orbital period for a planet
-func orbitalPeriod(planet Planet) float64 {
-	switch planet {
-	case "Earth":
-		return 1
-	case "Mercury":
-		return 0.2408467
-	case "Venus":
-		return 0.61519726
-	case "Mars":
-		return 1.8808158
-	case "Jupiter":
-		return 11.862615
-	case "Saturn":
-		return 29.447498
-	case "Uranus":
-		return 84.016846
-	case "Neptune":
-		return 164.79132
-	default:
-		panic(fmt.Sprintf("Unknown planet %s", planet))
-	}
 }
