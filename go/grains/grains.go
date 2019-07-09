600c04170f29be53e1566ab0f48ab1f9efe273ec
Wil Boayue <wilfred.boayue@airbus-sv.com>
2019-07-09 15:16:21 -0700

    go/grains

---
+++ go/grains/grains.go
@@ -0,0 +1,31 @@
+package grains
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+// Calculates number of grains on square
+func Square(x int) (uint64, error) {
+	if x < 1 || x > 64 {
+		return 0, fmt.Errorf("expected 1 to 64 got %d", x)
+	}
+
+	return uint64(math.Pow(2, float64(x-1))), nil
+}
+
+// Calculates total number of grains
+func Total() uint64 {
+	return totalUpTo(64)
+}
+
+// total up to square x is 2^x - 1
+func totalUpTo(x int64) uint64 {
+	var i, e = big.NewInt(2), big.NewInt(x)
+
+	i = i.Exp(i, e, nil)
+	i = i.Sub(i, big.NewInt(1))
+
+	return i.Uint64()
+}
